e3ecbfcfc3d9c987aaddc1255f67b2bfed1719db
Akshay Bhatt <akshay.bhatt@hotmail.com>
2021-08-02 12:15:57 +0530

    Update server.js
    server changes for keypress timeout functionality

--- server.js
+++ server.js
@@ -9,50 +9,34 @@ app.get('/', function(request,response)
 
 });
 
-app.get("/server",function(request,response)
-{
+app.get("/server",function(request,response){
 	var data = request.query.search.toLowerCase();
-	fs.readFile("detail.json","utf8",function(error,fileData)
-	{
-		if(error)
-		{
+	fs.readFile("detail.json","utf8",function(error,fileData){
+		if(error){
 			console.error(error);
 			response.sendStatus(500);
-		}
-		else
-		{	
-			var jsonContent =JSON.parse(fileData)
+		}else{	
+			var jsonContent =JSON.parse(fileData);
+			
 			var dataLength = data.length,
 			stringLength,responseData =[],lowerJsonData;
-			if(dataLength != 0)
-			{
+			if(dataLength != 0){
+				
 				var regex = new RegExp("^"+data,"g");
-				for(var i = 0; i<jsonContent.length;i++)
-				{ 
+				console.log(regex);
+				for(var i = 0; i<jsonContent.length;i++){ 
 					stringLength = jsonContent[i].name.length;
 					lowerJsonData =jsonContent[i].name.toLowerCase();
-					if(lowerJsonData.match(regex))
-					{
-						responseData.push(jsonContent[i]);
-						
-					}
-					else
-					{
-						responseData.push(jsonContent[i]);
-					}				      	
+					if(lowerJsonData.match(regex)){
+						responseData.push(jsonContent[i]);	
+					}      	
 				}
 				response.send(responseData);		
-			}
-			else
-			{
+			}else{
 				response.send([]);
-			}
-			
-		
-			
+			}	
 		}
-	});
-	
+	});	
 });
 app.listen(4000,function(){
 	console.log("the server is running on port 4000");
