cd6f77541c41c135fdd90754915ba87471516fe7
Akshay Bhatt <akshay.bhatt@hotmail.com>
2021-08-02 12:16:49 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,17 +1,30 @@
+var inputValue='',timer='';
 $("#input").on("keyup",function(){
-     var inputValue = $( "#input" ).val();
-$.ajax({
-  method: "GET",
-  url: "http://localhost:4000/server?search="+inputValue,
-}) .done(function( msg ) {
-      var json_obj = msg;
-     var output="";
-    for (var i=0 ;i<json_obj.length;i++) 
-            {
-                output +="<h2>" + json_obj[i].name + "</h2><h3>" + json_obj[i].detail+ "</h3>";
-                
-            }
-            $('#search-result').html(output);
-  });
 
+  inputValue = $( "#input" ).val();
+  console.log("keyup");
+  timer = setTimeout(doneTyping, 2000); 
 });
+
+$('#input').on('keydown',function(){
+  console.log("down");
+  clearTimeout(timer);
+  
+})
+
+function doneTyping(){
+
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:4000/server?search="+inputValue,
+  }) .done(function( msg ) {
+        var json_obj = msg;
+       var output="";
+      for (var i=0 ;i<json_obj.length;i++) {
+                  output +="<h2>" + json_obj[i].name + "</h2><h3>" + json_obj[i].detail+ "</h3>";
+                  
+              }
+              $('#search-result').html(output);
+    });
+
+}
